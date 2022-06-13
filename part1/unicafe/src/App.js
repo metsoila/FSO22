ea112cfc1accf83100b1d2fb7d8bbf20bbdbb9f7
metsoila <tuomas.metsoila@gmail.com>
2022-06-13 16:58:24 +0300

    1.6 added

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,21 +1,25 @@
 import { useState } from 'react'
 
-const App = () => {
 
+const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setAll(all + 1)
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setAll(all + 1)
   }
 
   return (
@@ -33,6 +37,9 @@ const App = () => {
         <p> good {good}</p>
         <p> neutral {neutral}</p>
         <p> bad {bad}</p>
+        <p> all {all} </p>
+        <p> average {(good - bad)/all} </p>
+        <p> positive {100*good/all} % </p>
       </div>
     </div>
   )

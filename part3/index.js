4c38ac42a0b660fb0190671eb2e535960e57f7bf
metsoila <tuomas.metsoila@gmail.com>
2023-03-02 00:18:46 +0200

    3.4

--- part3/index.js
+++ part3/index.js
@@ -33,15 +33,22 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+  if (person) {
+      response.json(person)
+  } else {
+      response.status(404).end()
+  }
 
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 app.get('/info', (request, response) => {
